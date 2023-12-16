cce45920c2eccc92b87608554b87bdee0acfed32
mukilan2815 <mukilan2808@gmail.com>
2023-12-16 21:44:59 +0530

    bug

--- Pages/FilterPage.js
+++ Pages/FilterPage.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, FlatList, SafeAreaView } from 'react-native';
+import { View, Text, TouchableOpacity, FlatList, SafeAreaView, StyleSheet } from 'react-native';
 import { Button, List, Divider } from 'react-native-paper';
 
 const FilterPage = () => {
-  const categories = ['Category', 'Brand', 'Color', 'Price', 'Reviews', 'Delivery'];
+  const categories = ['Category', 'Brand', 'Price', 'Reviews', 'Delivery'];
 
   const [selectedCategory, setSelectedCategory] = useState('');
   const [filterOptions, setFilterOptions] = useState([]);
@@ -11,14 +11,13 @@ const FilterPage = () => {
   const sampleData = {
     Category: ['Electronics', 'Clothing', 'Shoes', 'Accessories'],
     Brand: ['Nike', 'Adidas', 'Samsung', 'Apple'],
-    Color: ['Red', 'Blue', 'Green', 'Black', 'White'],
     Price: ['Under $25', '$25 - $50', '$50 - $100', '$100 and above'],
     Reviews: ['⭐', '⭐⭐', '⭐⭐⭐', '⭐⭐⭐⭐', '⭐⭐⭐⭐⭐'],
     Delivery: ['Next Day Delivery', 'Express Shipping', 'Standard Shipping'],
   };
 
   const renderFilterOptions = ({ item }) => (
-    <TouchableOpacity style={{ marginBottom: 10 }}>
+    <TouchableOpacity style={styles.filterOption}>
       <Button mode="contained">{item}</Button>
     </TouchableOpacity>
   );
@@ -29,18 +28,19 @@ const FilterPage = () => {
   };
 
   return (
-    <SafeAreaView style={{ flex: 1,marginVertical:30, }}>
-      <View style={{ flex: 1, flexDirection: 'row' }}>
-        <View style={{ flex: 1, backgroundColor: '#f0f0f0' }}>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.mainContainer}>
+        {/* Left Section */}
+        <View style={styles.leftSection}>
           <List.Section>
-            <List.Subheader>Filter Categories</List.Subheader>
+            <List.Subheader>Filter Category</List.Subheader>
             <FlatList
               data={categories}
               renderItem={({ item, index }) => (
                 <TouchableOpacity
                   key={index}
                   onPress={() => handleCategoryPress(item)}
-                  style={{ padding: 10 }}
+                  style={styles.categoryItem}
                 >
                   <List.Item title={item} />
                 </TouchableOpacity>
@@ -50,25 +50,67 @@ const FilterPage = () => {
           </List.Section>
         </View>
 
-        <View style={{ flex: 2, borderLeftWidth: 1, borderColor: '#ccc', padding: 10 }}>
+        {/* Right Section */}
+        <View style={styles.rightSection}>
           {selectedCategory ? (
             <>
-              <Text style={{ fontSize: 18, marginBottom: 10 }}>{selectedCategory} Options:</Text>
+              <Text style={styles.optionsHeader}>{selectedCategory} Options:</Text>
               <Divider />
               <FlatList
                 data={filterOptions}
                 renderItem={renderFilterOptions}
                 keyExtractor={(item, index) => index.toString()}
-                style={{ flex: 1 }}
+                style={styles.filterOptionsList}
               />
             </>
           ) : (
-            <Text>Please select a category to see options.</Text>
+            <Text style={styles.selectCategoryText}>Please select a category to see options.</Text>
           )}
         </View>
       </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginVertical: 30,
+  },
+  mainContainer: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  leftSection: {
+    width: 150,
+    backgroundColor: '#C3C3C3',
+    height: '100%',
+    paddingBottom: 20,
+  },
+  categoryItem: {
+    padding: 10,
+  },
+  rightSection: {
+    flex: 2,
+    borderLeftWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    backgroundColor: '#F5F5F5',
+  },
+  optionsHeader: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  filterOptionsList: {
+    flex: 1,
+  },
+  filterOption: {
+    marginBottom: 10,
+  },
+  selectCategoryText: {
+    textAlign: 'center',
+    marginTop: 20,
+  },
+});
 
 export default FilterPage;
